77861c44ac1054e4a168ea524c5f5778e4421f52
john-kelly <johnkellytemena@gmail.com>
2025-02-25 00:50:25 +0800

    Add function for checking if game is over

--- script.js
+++ script.js
@@ -13,14 +13,70 @@ function createGameboard() {
     function placeMark(x, y, mark) {
         gameboard[x][y] = mark;
     };
+    
+    function isBoardEmpty() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (gameboard[i][j] !== null) return false;
+            }
+        }
 
-    return {getBoard, placeMark};
+        return true;
+    }
+
+    function isGameOver() {
+
+        for (let i = 0; i < 3; i++) {
+            // check rows
+            if (gameboard[i][0] !== null && gameboard[i][0] === gameboard[i][1] && gameboard[i][1] === gameboard[i][2]) {
+                return true;
+            }   
+
+            // check columns
+            if (gameboard[0][i] !== null && gameboard[0][i] === gameboard[1][i] && gameboard[1][i] === gameboard[2][i]) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (gameboard[1][1] !== null) {
+            if (
+                (gameboard[0][0] === gameboard[1][1] && gameboard[1][1] === gameboard[2][2]) ||
+                (gameboard[0][2] === gameboard[1][1] && gameboard[1][1] === gameboard[2][0])
+            ) {
+                return true;
+            }
+        }
+
+        // check if board is full
+        for (let i = 0; i < 3; i++) {
+            if (gameboard[i].includes(null)) {
+                return false;
+            } else if (i === 3) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    return {getBoard, placeMark, isBoardEmpty, isGameOver};
 }
 
 let game = createGameboard();
 
-console.log(game.getBoard());
-
 game.placeMark(0, 0, "X");
+game.placeMark(0, 1, "O");
+game.placeMark(0, 2, "X");
+
+game.placeMark(1, 0, "X");
+game.placeMark(1, 1, "O");
+game.placeMark(1, 2, "X");
+
+game.placeMark(2, 0, "O");
+game.placeMark(2, 1, "X");
+game.placeMark(2, 2, "O");
+
+console.log(game.isGameOver());
 console.log(game.getBoard());
 
